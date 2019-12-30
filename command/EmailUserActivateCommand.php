33823e771ef90c08df54437a13e4a954999ddb4e
jonathan-martz <github@jmartz.de>
2019-12-30 18:48:19 +0100

    add logic for email command ,composer update

--- command/EmailUserActivateCommand.php
+++ command/EmailUserActivateCommand.php
@@ -2,7 +2,11 @@
 
 namespace App\Console\Commands;
 
+use App\Mail\RegisterUser;
+use App\Model\User;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 
 class EmailUserActivateCommand extends Command
 {
@@ -32,6 +36,29 @@ class EmailUserActivateCommand extends Command
 
     public function handle()
     {
-        var_dump('test');
+        $users = DB::table('users')->where('active', '=', '0');
+
+        if ($users->count() !== 0) {
+            foreach ($users->get() as $key => $user) {
+                $user = new User((array)$user);
+
+                $tokens = DB::table('user_activate_token')
+                    ->where('userid', '=', $user->getAuthIdentifier());
+
+                if ($tokens->count() === 0) {
+                    $token = bin2hex(openssl_random_pseudo_bytes(256));
+                    DB::table('user_activate_token')->insert(
+                        [
+                            'userid' => $user->getAuthIdentifier(),
+                            'token' => $token,
+                            'date' => strtotime('now')
+                        ]
+                    );
+                    Mail::to($user->getAttribute('email'))
+                        ->send(new RegisterUser($user->getAttribute('email'), $token, $user->getAttribute('email')));
+                    var_dump('Mail send.');
+                }
+            }
+        }
     }
 }
